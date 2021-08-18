d29a1c2b92f09000a50c1f694f1163bfdea65aea
Dane Russell <danerussell@danes-air.lan>
2021-08-17 20:30:05 -0400

    fix(App.test): boilerplate removal and real test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a star wars character', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('star-wars-character');
   expect(linkElement).toBeInTheDocument();
 });
