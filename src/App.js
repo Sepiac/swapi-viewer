ea8bb0d2cdd98c4f0449a3f2e590bcb529817883
Dane Russell <danerussell@danes-air.lan>
2021-08-17 20:29:32 -0400

    fix(App): infinite rendering issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import StarWarsCharacter from './Components/StarWarsCharacter';
 
 function App() {
   const [character, setCharacter] = useState({})
 
-  fetch('https://swapi.dev/api/people/1/').then(res => res.json()).then(character => {
-    setCharacter(character)
-  });
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/1/').then(res => res.json()).then(character => {
+      setCharacter(character)
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
