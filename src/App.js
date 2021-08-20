54c4ba0dad05c6e45c119b69c28e3e0a49eb3131
Dane Russell <danerussell@danes-air.lan>
2021-08-19 20:43:26 -0400

    style(app): formatting

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import {useGetStarWarsCharacter, useGetStarWarsWorld} from './hooks';
 import { StarWarsCharacter, StarWarsWorld } from './Components';
 
 function App() {
-  
+
   const { character } = useGetStarWarsCharacter(1);
   const { world } = useGetStarWarsWorld(character?.homeworld);
 
   return (
     <div className="App">
       <header className="App-header">
-        <StarWarsCharacter character={character}/>
+        <StarWarsCharacter character={character} />
         <StarWarsWorld world={world} />
       </header>
     </div>
