e2dd066f04a4db9573345cff2494b3dc7ab33b55
Dane Russell <danerussell@danes-air.lan>
2021-08-24 23:26:07 -0400

    feat(app): add top level tabs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useSwapiApi } from './hooks';
-import { StarWarsCharacter, StarWarsWorld } from './Components';
-import { Spinner } from 'reactstrap';
+import { StarWarsCharacter } from './Components';
+import { Container, Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, Card, CardTitle, CardText, Button, Spinner } from 'reactstrap';
+import classnames from 'classnames';
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
 
   const { data: character } = useSwapiApi({
     entity: 'people',
     id: 1
   });
 
-  const { data: world } = useSwapiApi({ url: character?.homeworld });
-
   return (
     <div className="App">
-      <header className="App-header">
-        {character && world ? (
-          <>
-            <StarWarsCharacter character={character} />
-            {/* <StarWarsWorld world={world} /> */}
-          </>
-        ) : (
-          <Spinner color="primary" />
-        )}
-      </header>
+      <Container style={{'margin-top': '25px'}}>
+        <div>
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggle('1'); }}
+              >
+                Characters
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggle('2'); }}
+              >
+                Worlds
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <StarWarsCharacter character={character} />
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="6">
+                  <Card body>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Go somewhere</Button>
+                  </Card>
+                </Col>
+                <Col sm="6">
+                  <Card body>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Go somewhere</Button>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
+      </Container>
     </div>
   );
 }
