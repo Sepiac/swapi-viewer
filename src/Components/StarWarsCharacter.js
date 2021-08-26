cc774c3a40ebf9aad085da386d8307297c68481b
Dane Russell <danerussell@danes-air.lan>
2021-08-25 21:06:44 -0400

    feat(character): show spinner when loading

--- src/Components/StarWarsCharacter.js
+++ src/Components/StarWarsCharacter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
+import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter, Spinner } from 'reactstrap'
 
 const StarWarsCharacter = ({ character }) => {
 
@@ -19,24 +19,29 @@ const StarWarsCharacter = ({ character }) => {
     return gender === 'male' ? 'he' : gender === 'female' ? 'she' : 'they';
   }
 
-  const capitalizeFirstLetter = (string='') => {
+  const capitalizeFirstLetter = (string = '') => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   return (
-    <Card>
-      <CardHeader>{name}</CardHeader>
-      <CardBody>
-        <CardTitle tag="h5">Description</CardTitle>
-        <CardText>
-          {capitalizeFirstLetter(name)} has {hairColor} hair with {skinColor} skin and {eyeColor} eyes.
-        </CardText>
-        <CardText>
-          {capitalizeFirstLetter(getPronoun())} {(gender !== 'male' && gender !== 'female') ? 'are' : 'is'} {height}cm tall and weighs {mass} kilograms.
-        </CardText>
-      </CardBody>
-      <CardFooter>born {birthYear}</CardFooter>
-    </Card>
+    <>
+      {character ? (
+        <Card>
+          <CardHeader>{name}</CardHeader>
+          <CardBody>
+            <CardTitle tag="h5">Description</CardTitle>
+            <CardText>
+              {capitalizeFirstLetter(name)} has {hairColor} hair with {skinColor} skin and {eyeColor} eyes.
+            </CardText>
+            <CardText>
+              {capitalizeFirstLetter(getPronoun())} {(gender !== 'male' && gender !== 'female') ? 'are' : 'is'} {height}cm tall and weighs {mass} kilograms.
+            </CardText>
+          </CardBody>
+          <CardFooter>born {birthYear}</CardFooter>
+        </Card>
+      ) : (<Spinner color="primary" />)}
+    </>
+
   )
 }
 
