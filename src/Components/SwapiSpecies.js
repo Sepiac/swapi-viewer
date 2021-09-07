dcd6e75ca496c52a0fd23a7e63b5e8b78c98e885
Dane Russell <danerussell4@gmail.com>
2021-09-06 22:14:27 -0400

    feat(species): add detail

--- src/Components/SwapiSpecies.js
+++ src/Components/SwapiSpecies.js
@@ -1,17 +1,53 @@
 import React from 'react'
+import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter, Spinner } from 'reactstrap'
 import PropTypes from 'prop-types'
 
 const SwapiSpecies = props => {
-  const {species} = props;
+  const { species } = props;
+
+  const {
+    name,
+    classification,
+    designation,
+    average_height: averageHeight,
+    skin_colors: skinColors,
+    hair_colors: hairColors,
+    eye_colors: eyeColors,
+    average_lifespan: averageLifespan,
+    language,
+    detail,
+  } = species;
+
+
   return (
-    <div>
-      {JSON.stringify(species)}
-    </div>
+    <Card data-testid="swapi-person">
+      {detail ? (
+        <CardBody>
+          <CardText>
+            {detail}
+          </CardText>
+        </CardBody>
+      ) : (
+        <>
+          <CardHeader data-testid="header-section">{name}</CardHeader>
+          <CardBody>
+            <CardTitle tag="h5">Description</CardTitle>
+            <CardText data-testid="description-section-1">
+              {name}s are a {designation} species with an average lifespan of {averageLifespan} years.
+            </CardText>
+            <CardText data-testid="description-section-2">
+              The average height of a(n) {name} individual is {averageHeight}cm.
+            </CardText>
+          </CardBody>
+          <CardFooter data-testid="footer-section">{name}: ({classification})</CardFooter>
+        </>
+      )}
+    </Card>
   )
 }
 
 SwapiSpecies.propTypes = {
-
+  species: PropTypes.object
 }
 
 export default SwapiSpecies
