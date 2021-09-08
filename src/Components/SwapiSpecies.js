cb159c35e94b8b5a82fd4e1a5ba881b52788c887
Dane Russell <danerussell4@gmail.com>
2021-09-07 22:43:38 -0400

    refactor(species): import cleanup

--- src/Components/SwapiSpecies.js
+++ src/Components/SwapiSpecies.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter, Spinner } from 'reactstrap'
+import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
 import PropTypes from 'prop-types'
 import { startsWithVowel } from '../utils'
 
