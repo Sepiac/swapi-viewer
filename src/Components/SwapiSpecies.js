c0c5832014b69cc3d2dffedd89edfb9e893b9946
Dane Russell <danerussell4@gmail.com>
2021-09-08 18:03:23 -0400

    fix(species): make nullsafe

--- src/Components/SwapiSpecies.js
+++ src/Components/SwapiSpecies.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import { startsWithVowel } from '../utils'
 
 const SwapiSpecies = props => {
-  const { species } = props
+  const { species={} } = props
 
   const {
     name,
