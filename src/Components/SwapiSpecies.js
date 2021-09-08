eedd114c227fdf7906cd4d5ac09b8dc46575f227
Dane Russell <danerussell4@gmail.com>
2021-09-07 20:18:17 -0400

    refactor(components): remove loading resolves gh-30

--- src/Components/SwapiSpecies.js
+++ src/Components/SwapiSpecies.js
@@ -16,33 +16,22 @@ const SwapiSpecies = props => {
     eye_colors: eyeColors,
     average_lifespan: averageLifespan,
     language,
-    detail,
   } = species;
 
 
   return (
     <Card data-testid="swapi-person">
-      {detail ? (
-        <CardBody>
-          <CardText>
-            {detail}
-          </CardText>
-        </CardBody>
-      ) : (
-        <>
-          <CardHeader data-testid="header-section">{name}</CardHeader>
-          <CardBody>
-            <CardTitle tag="h5">Description</CardTitle>
-            <CardText data-testid="description-section-1">
-              {name}s are a {designation} species with an average lifespan of {averageLifespan} years.
-            </CardText>
-            <CardText data-testid="description-section-2">
-              The average height of {startsWithVowel(name) ? 'an' : 'a'} {name} individual is {averageHeight}cm.
-            </CardText>
-          </CardBody>
-          <CardFooter data-testid="footer-section">{name}: ({classification})</CardFooter>
-        </>
-      )}
+      <CardHeader data-testid="header-section">{name}</CardHeader>
+      <CardBody>
+        <CardTitle tag="h5">Description</CardTitle>
+        <CardText data-testid="description-section-1">
+          {name}s are a {designation} species with an average lifespan of {averageLifespan} years.
+        </CardText>
+        <CardText data-testid="description-section-2">
+          The average height of {startsWithVowel(name) ? 'an' : 'a'} {name} individual is {averageHeight}cm.
+        </CardText>
+      </CardBody>
+      <CardFooter data-testid="footer-section">{name}: ({classification})</CardFooter>
     </Card>
   )
 }
--- src/Components/__tests__/SwapiPlanet.test.js
+++ src/Components/__tests__/SwapiPlanet.test.js
@@ -4,18 +4,6 @@ import {render, screen} from '@testing-library/react'
 import SwapiPlanet from '../SwapiPlanet'
 import planetResponse from './planet.response.json'
 
-describe('when no data is passed', () => {
-  it('should render loading indicator when nothing is passed', () => {
-    render (<SwapiPlanet />);
-    expect(screen.getByTestId('loading-indicator')).toBeInTheDocument();
-  })
-  
-  it('should render when passed an empty object', () => {
-    render (<SwapiPlanet planet={{}}/>);
-    expect(screen.getByTestId('loading-indicator')).toBeInTheDocument();
-  })
-});
-
 describe('header', () => {
   it('should render name in header', () => {
     render(<SwapiPlanet planet={planetResponse} />);
