aa3038d96e37d7eb412517596099148b4e879598
Dane Russell <danerussell4@gmail.com>
2021-09-11 12:48:14 -0400

    feat(SwapiFilm): add component resolves gh-21

---
+++ src/Components/SwapiFilm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, CardHeader, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
+import { getFormattedDate } from '../utils'
+import romanNumeral from 'roman-numeral'
+
+const SwapiFilm = ({ film = {} }) => {
+  const {
+    title,
+    episode_id: episodeId,
+    opening_crawl: openingCrawl,
+    director,
+    producer,
+    release_date: releaseDate,
+  } = film
+
+  return (
+      <Card data-testid="swapi-film">
+        <CardHeader data-testid="header-section">Episode {romanNumeral.convert(episodeId)}: {title}</CardHeader>
+        <CardBody>
+          <CardTitle tag="h5">Description</CardTitle>
+          <CardText data-testid="description-section-1">
+            {title} was directed by {director}, produced by {producer}, and released on {getFormattedDate(releaseDate)}.
+          </CardText>
+          <CardText data-testid="description-section-2">
+            {openingCrawl}
+          </CardText>
+        </CardBody>
+        <CardFooter data-testid="footer-section">Episode {romanNumeral.convert(episodeId)}: {title}</CardFooter>
+      </Card>
+  )
+}
+
+SwapiFilm.propTypes = {
+  film: PropTypes.object
+}
+
+export default SwapiFilm
