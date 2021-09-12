a78c3624d2245f30a84750e7a1d72d26aaf40bfd
Dane Russell <danerussell4@gmail.com>
2021-09-12 17:26:50 -0400

    test(SwapiPerson): test text equality

--- src/Components/SwapiPerson/__tests__/SwapiPerson.test.js
+++ src/Components/SwapiPerson/__tests__/SwapiPerson.test.js
@@ -6,7 +6,7 @@ import personResponse from '../__tests__/person.response.json'
 describe('header', () => {
   it('should render name in header', () => {
     render(<SwapiPerson person={personResponse} />);
-    expect(screen.getByTestId('header-section')).toHaveTextContent('Luke Skywalker');
+    expect(screen.getByTestId('header-section').textContent).toEqual('Luke Skywalker');
   });
 })
 
@@ -17,7 +17,7 @@ describe('description-section-1', () => {
 
   it('should render the correct message', () => {
     const message = 'Luke Skywalker has blond hair with fair skin and blue eyes.'
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent(message)
+    expect(screen.getByTestId('description-section-1').textContent).toEqual(message)
   });
 });
 
@@ -28,13 +28,13 @@ describe('description-section-2', () => {
 
   it('should render the correct message', () => {
     const message = 'He is 172cm tall and weighs 77 kilograms.'
-    expect(screen.getByTestId('description-section-2')).toHaveTextContent(message)
+    expect(screen.getByTestId('description-section-2').textContent).toEqual(message)
   });
 });
 
 describe('footer', () => {
   it('should render the birth year in the footer', () => {
     render(<SwapiPerson person={personResponse} />);
-    expect(screen.getByTestId('footer-section')).toHaveTextContent('born 19BBY');
+    expect(screen.getByTestId('footer-section').textContent).toEqual('born 19BBY');
   });
 })
