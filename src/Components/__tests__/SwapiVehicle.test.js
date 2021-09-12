307f6d2b2f7f245d9a31bb67835956338aa9cb21
Dane Russell <danerussell4@gmail.com>
2021-09-12 16:40:36 -0400

    test(SwapiVehicle): add unit tests

---
+++ src/Components/__tests__/SwapiVehicle.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import SwapiVehicle from '../SwapiVehicle'
+import vehicleResponse from './vehicle.response.json'
+
+describe('header', () => {
+  it('should render name in header', () => {
+    render(<SwapiVehicle vehicle={vehicleResponse} />);
+    expect(screen.getByTestId('header-section')).toHaveTextContent('Sand Crawler')
+  })
+})
+
+describe('description-section-1', () => {
+  beforeEach(() => {
+    render(<SwapiVehicle vehicle={vehicleResponse} />);
+  })
+
+  it('should render the correct message', () => {
+    const message = 'The Sand Crawler is a wheeled class Corellia Mining Corporation Digger Crawler and is worth 150000 credits.'
+    expect(screen.getByTestId('description-section-1')).toHaveTextContent(message)
+  })
+})
+
+describe('description-section-2', () => {
+  beforeEach(() => {
+    render(<SwapiVehicle vehicle={vehicleResponse} />);
+  })
+
+  it('should render the correct message', () => {
+    const message = 'This vehicle can carry 30 passengers, 50000kg of cargo, and 2 months of provisions.'
+    expect(screen.getByTestId('description-section-2')).toHaveTextContent(message)
+  })
+})
+
+describe('footer', () => {
+  it('should render the correct footer', () => {
+    render(<SwapiVehicle vehicle={vehicleResponse} />);
+    expect(screen.getByTestId('footer-section')).toHaveTextContent('Sand Crawler: (wheeled)')
+  });
+})
