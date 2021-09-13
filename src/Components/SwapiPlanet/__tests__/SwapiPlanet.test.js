3752887a1ac1e22f3f91d03cd7df022a34c0a415
Dane Russell <danerussell4@gmail.com>
2021-09-12 22:41:06 -0400

    test(SwapiPlanet): restyle unit tests

--- src/Components/SwapiPlanet/__tests__/SwapiPlanet.test.js
+++ src/Components/SwapiPlanet/__tests__/SwapiPlanet.test.js
@@ -7,7 +7,7 @@ import planetResponse from './planet.response.json'
 describe('header', () => {
   it('should render name in header', () => {
     render(<SwapiPlanet planet={planetResponse} />);
-    expect(screen.getByTestId('header-section')).toHaveTextContent('Tatooine');
+    expect(screen.getByTestId('header-section').textContent).toEqual('Tatooine');
   });
 })
 
@@ -16,26 +16,15 @@ describe('description-section-1', () => {
     render(<SwapiPlanet planet={planetResponse} />);
   });
 
-  it('should render the name', () => {
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent('Tatooine');
-  });
-  it('should render the terrain', () => {
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent('desert');
-  });
-  it('should render the climate', () => {
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent('arid');
-  });
-  it('should render the day length', () => {
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent('23 hours');
-  });
-  it('should render the year length', () => {
-    expect(screen.getByTestId('description-section-1')).toHaveTextContent('304 days');
+  it('should render the correct message', () => {
+    const message = 'Tatooine is a desert planet with an arid climate, a day length of 23 hours, and year length of 304 days.'
+    expect(screen.getByTestId('description-section-1').textContent).toEqual(message)
   });
 });
 
 describe('footer', () => {
   it('should render the birth year in the footer', () => {
     render(<SwapiPlanet planet={planetResponse} />);
-    expect(screen.getByTestId('footer-section')).toHaveTextContent('Tatooine: arid, desert');
+    expect(screen.getByTestId('footer-section').textContent).toEqual('Tatooine: arid, desert');
   });
 })
