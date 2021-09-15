ffdcf713e9c92a28d808a5b760cd0ac889f47bab
Dane Russell <danerussell4@gmail.com>
2021-09-14 23:06:38 -0400

    feat(useSwapiApi): handle http errors

--- src/hooks/useSwapiApi.js
+++ src/hooks/useSwapiApi.js
@@ -1,9 +1,13 @@
 import { useQuery } from 'react-query'
 
 const useSwapiApi = ({ entity, id, url }) => {
-    return useQuery(`${entity}:${id}`, () => (
-        fetch(url ? url : `https://swapi.dev/api/${entity}/${id}/`).then(res => res.json())
-    ), {
+    return useQuery(`${entity}:${id}`, async () => {
+        const response = await fetch(url ? url : `https://swapi.dev/api/${entity}/${id}/`)
+        if(!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    }, {
         staleTime: (1000 * 60) * 60
     })
 }
