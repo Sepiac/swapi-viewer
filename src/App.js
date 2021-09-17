83d65fa68cbdd4af56c8219c33ec6538710d0213
Dane Russell <danerussell4@gmail.com>
2021-09-17 19:42:40 -0400

    feat(SwapiNavBar): add navbar resolves gh-58

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { SwapiTabView } from './Components'
+import { SwapiTabView, SwapiNavBar } from './Components'
 
 function App() {
 
   return (
     <div className="App">
       <QueryClientProvider client={new QueryClient()}>
+        <SwapiNavBar />
         <SwapiTabView />
       </QueryClientProvider>
     </div>
