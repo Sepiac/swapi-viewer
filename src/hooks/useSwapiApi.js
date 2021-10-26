30f1c231ea891e7a1cb5fae19aba35f2defb974c
Dane Russell <danerussell4@gmail.com>
2021-10-26 00:22:04 -0400

    refactor(useSwapiApi): switch to axios resolves gh-63

--- src/hooks/useSwapiApi.js
+++ src/hooks/useSwapiApi.js
@@ -1,12 +1,17 @@
+import axios from 'axios'
 import { useQuery } from 'react-query'
+
+const swapiAxios = ({ entity, id, url }) => {
+    return axios.get(url ? url : `https://swapi.dev/api/${entity}/${id}/`);
+}
 
 const useSwapiApi = ({ entity, id, url }) => {
     return useQuery(`${entity}:${id}`, async () => {
-        const response = await fetch(url ? url : `https://swapi.dev/api/${entity}/${id}/`)
-        if(!response.ok) {
+        const response = await swapiAxios({ entity, id, url })
+        if(!response.status === 200) {
             throw new Error(`Error ${response.status} when fetching ${entity} id ${id}.`)
         }
-        return response.json()
+        return response.data
     }, {
         staleTime: (1000 * 60) * 60
     })
